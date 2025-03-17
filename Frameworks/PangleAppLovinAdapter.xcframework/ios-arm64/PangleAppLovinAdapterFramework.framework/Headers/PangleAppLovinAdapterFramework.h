39bb88db56345488a25fbabf09ad8b528edfc973
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:09:36 +0200

    First commit

---
+++ Frameworks/PangleAppLovinAdapter.xcframework/ios-arm64/PangleAppLovinAdapterFramework.framework/Headers/PangleAppLovinAdapterFramework.h
@@ -0,0 +1,18 @@
+//
+//  PangleAppLovinAdapterFramework.h
+//  PangleAppLovinAdapterFramework
+//
+//  Created by Robert Tataru on 10.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PangleAppLovinAdapterFramework.
+FOUNDATION_EXPORT double PangleAppLovinAdapterFrameworkVersionNumber;
+
+//! Project version string for PangleAppLovinAdapterFramework.
+FOUNDATION_EXPORT const unsigned char PangleAppLovinAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PangleAppLovinAdapterFramework/PublicHeader.h>
+
+
